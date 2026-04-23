1c798dc92128d88c4ad2fa6f6cc706b878bc2ca0
tichma <tichma@vk.com>
2026-04-23 13:39:27 +0300

    fix checkVoice returning voice_was_sent = true despite tg_id being admin

--- config/app.php
+++ config/app.php
@@ -98,7 +98,7 @@ return [
     'cipher' => 'AES-256-CBC',
 
     'key' => env('APP_KEY'),
-    'admin_list' => env('ADMIN_TELEGRAM_ID_LIST'),
+    'admin_list' =>  array_map('trim', explode(',', env('ADMIN_TELEGRAM_ID_LIST', ''))),
 
     'previous_keys' => [
         ...array_filter(
